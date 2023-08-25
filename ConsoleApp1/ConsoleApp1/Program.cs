423fb2e250ee54c4df5ab7618d96005ad1856510
Predator <murad.mammedzade11@gmail.com>
2023-08-25 03:40:24 -0700

    fjj

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDateStr = Console.ReadLine();
+            DateTime inputDate = DateTime.ParseExact(inputDateStr, "dd/MM/yyyy", null);
+            DateTime newDate = inputDate.AddDays(30).AddHours(6).AddMinutes(17);
+            Console.WriteLine(newDate.ToString("dd/MM/yyyy"));
+        }
+    }
+}
